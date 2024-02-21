cd4da3c95ab61940110f5e8617cadb80ed2eb313
Eduardo Castillo DXC <e.castillo4@dxc.com>
2024-02-20 20:28:34 -0600

    cambio producción

--- src/todos/use-cases/render-pending.js
+++ src/todos/use-cases/render-pending.js
@@ -7,11 +7,15 @@ let element;
  * @param {String} elementId 
  */
 export const renderPendingTodos = ( elementId ) => {
+
+    console.log('Renderizando pendientes', elementId);
+
     if (!element)
         element = document.querySelector( elementId );
     
     if (!element)
         throw new Error(`Elemento  ${ elementId } not found`);
 
-    element.innerHMTL = todoStore.getTodos( Filters.Pending ).length;
+    element.innerHTML = todoStore.getTodos( Filters.Pending ).length;
+
 }
